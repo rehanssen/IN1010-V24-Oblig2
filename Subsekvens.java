9aac500397e8522e23afc7cf84dca6a674e566fc
Robert Edin Hanssen <96841359+rehanssen@users.noreply.github.com>
2024-04-22 16:51:43 +0000

    Implementert Subsekvens.java

--- Subsekvens.java
+++ Subsekvens.java
@@ -1,3 +1,15 @@
 public class Subsekvens {
-    //TODO: implementer klassen
+    protected String subsekvens;
+    protected final int antall;
+
+    public Subsekvens(String s) {
+        this.subsekvens = s;
+        antall = 1;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.subsekvens + "," + this.antall + ")";
+    }
+
 }
